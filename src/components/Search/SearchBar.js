d21d864faf76d1f726aac8382796a8d5e68dc1a3
mcktivity <mcktivity@gmail.com>
2020-09-12 17:26:18 +0200

    Exercise2. added handleReset and useState

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -3,8 +3,16 @@ import ClearIcon from "./../../assets/cross.svg";
 import SearchIcon from "./../../assets/magnifying-glass.svg";
 
 export default () => {
+  const [isTyping, setIsTyping] = useState(false);
+
   function handleSubmit(e) {
     e.preventDefault();
+  }
+
+  function handleReset(e) {
+    e.currentTarget.value = "";
+    setIsTyping(false);
+    // e.preventDefault();
   }
 
   return (
@@ -13,16 +21,24 @@ export default () => {
         role="search"
         className="search-form"
         onSubmit={(e) => handleSubmit(e)}
+        onReset={(e) => handleReset(e)}
       >
         <input
           type="text"
           id="searchBar-id"
           className="searchBar-input"
           placeholder="Search"
+          onChange={() => setIsTyping(true)}
         />
-        <button type="button">
-          <img src={SearchIcon} alt="search-icon" />
-        </button>
+        {!isTyping ? (
+          <button type="submit">
+            <img src={SearchIcon} alt="search-icon" />
+          </button>
+        ) : (
+          <button type="reset">
+            <img src={ClearIcon} alt="clear-icon" />
+          </button>
+        )}
       </form>
     </main>
   );
